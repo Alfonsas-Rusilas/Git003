725de14e0f8dd68e63c4828a885a9503b6da1149
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-02 22:22:05 +0300

    Startas

---
+++ C_003/C_003/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
